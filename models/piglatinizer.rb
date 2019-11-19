487d839a099937f73de77af8f5d8522a16d4d909
Victoria Hale <vic.hale74@gmail.com>
2019-11-19 21:25:49 +0000

    Automatically backed up by Learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -1,5 +1,5 @@
 class PigLatinizer
-  attr_accessor :words
+  attr_reader :words
   
   def initialize
     @words = words
@@ -10,14 +10,23 @@ class PigLatinizer
     b = a.map {|word| piglatinize_word(word)}
     b.join(" ")
   end
+  
   def piglatinize_word(word)
     first_letter = word[0].downcase
     if first_letter == "a" || first_letter == "e" || first_letter == "i" || first_letter == "o" || first_letter == "u"
         # piglatinize word that starts with a vowel
     else
-        # piglatinize word that starts with a consonant
+      consonants = []
+      consonants << word[0]
+        if ["a", "e", "i", "o", "u"].include?(word[1]) == false
+          consonants << word[1]
+          if ["a", "e", "i", "o", "u"].include?(word[2]) == false
+            consonants << word[2]
+          end
+        end
+      "#{word[consonants.length..-1] + consonants.join + "ay"}"
+    end# piglatinize word that starts with a consonant
   end
-end
 end
     
   # if starts with consonant take first letter and add to end of word + ay
