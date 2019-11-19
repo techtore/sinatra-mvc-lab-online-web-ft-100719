68ff66805189b6a136d632221e714de7194266b0
Victoria Hale <vic.hale74@gmail.com>
2019-11-19 21:37:34 +0000

    Automatically backed up by Learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -7,13 +7,14 @@ class PigLatinizer
 
   def piglatinize(word)
     parts_of_word = word.split(/([^aeiouAEIOU]*)([aeiouAEIOU]*)(.*)/)
-    binding.pry
+   
     start = parts_of_word[1] # consonant cluster
     rest = parts_of_word[2] + (parts_of_word[3] || "")
     if start.length>0
       "#{rest}#{start}ay"
     else
       "#{rest}way"
+       binding.pry
     end
   end
 end
