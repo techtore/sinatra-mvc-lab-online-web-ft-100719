48be127e40b53de75ccbb3b9ac4c2629463304c8
Victoria Hale <vic.hale74@gmail.com>
2019-11-19 21:34:15 +0000

    Automatically backed up by Learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -1,25 +1,19 @@
 class PigLatinizer
   
-  def piglatinize(string)
-    a = string.split(" ")
-    b = a.map {|word| piglatinize_word(word)}
-    b.join(" ")
+  def to_pig_latin(phrase)
+    words = phrase.split(" ")
+    words.collect {|w| piglatinize(w)}.join(" ")
   end
-  
-  def piglatinize_word(word)
-    first_letter = word[0].downcase
-    if first_letter == "a" || first_letter == "e" || first_letter == "i" || first_letter == "o" || first_letter == "u"
-        # piglatinize word that starts with a vowel
+
+  def piglatinize(word)
+    parts_of_word = word.split(/([^aeiouAEIOU]*)([aeiouAEIOU]*)(.*)/)
+    # binding.pry
+    start = parts_of_word[1] # consonant cluster
+    rest = parts_of_word[2] + (parts_of_word[3] || "")
+    if start.length>0
+      "#{rest}#{start}ay"
     else
-      consonants = []
-      consonants << word[0]
-        if ["a", "e", "i", "o", "u"].include?(word[1]) == false
-          consonants << word[1]
-          if ["a", "e", "i", "o", "u"].include?(word[2]) == false
-            consonants << word[2]
-          end
-        end
-      "#{word[consonants.length..-1] + consonants.join + "ay"}"
-    end# piglatinize word that starts with a consonant
+      "#{rest}way"
+    end
   end
 end
